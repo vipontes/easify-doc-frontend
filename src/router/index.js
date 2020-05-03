e37a8798ea23aca47d617e4e5aa5323f414aad55
viniciuspontes <vipontes70@gmail.com>
2020-05-03 09:16:05 -0300

    Modificações nas rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,86 +1,85 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
+import VueRouter from "vue-router";
 
 // Containers
-const TheContainer = () => import('@/containers/TheContainer')
+const TheContainer = () => import("@/containers/TheContainer");
 
 // Views
-const Dashboard = () => import('@/views/Dashboard')
+const Dashboard = () => import("@/views/Dashboard");
 
 // Views - Pages
-const Page404 = () => import('@/views/pages/Page404')
-const Page500 = () => import('@/views/pages/Page500')
-const Login = () => import('@/views/pages/Login')
-const Register = () => import('@/views/pages/Register')
+const Page404 = () => import("@/views/pages/Page404");
+const Page500 = () => import("@/views/pages/Page500");
+const Login = () => import("@/views/pages/Login");
+const Register = () => import("@/views/pages/Register");
 
-Vue.use(Router)
+Vue.use(Router);
 
 function validateUser() {
-
   return true;
 }
 
-function configRoutes () {
+function configRoutes() {
   return [
     {
-      path: '/',
-      redirect: '/dashboard',
-      name: 'Home',
-      beforeEnter (to, from, next) {
-        var auth = validateUser()
-        if (!auth) next({ name: 'Login' });
+      path: "/",
+      redirect: "/dashboard",
+      name: "Home",
+      beforeEnter(to, from, next) {
+        var auth = validateUser();
+        if (!auth) next({ name: "Login" });
         else next();
       },
       component: TheContainer,
       children: [
         {
-          path: 'dashboard',
-          name: 'Dashboard',
-          component: Dashboard
+          path: "dashboard",
+          name: "Dashboard",
+          component: Dashboard,
         },
-      ]
+      ],
     },
     {
-      path: '/pages',
-      redirect: '/pages/404',
-      name: 'Pages',
+      path: "/pages",
+      redirect: "/pages/404",
+      name: "Pages",
       component: {
-        render (c) { return c('router-view') }
+        render(c) {
+          return c("router-view");
+        },
       },
       children: [
         {
-          path: '404',
-          name: 'Page404',
-          component: Page404
+          path: "404",
+          name: "Page404",
+          component: Page404,
         },
         {
-          path: '500',
-          name: 'Page500',
-          component: Page500
+          path: "500",
+          name: "Page500",
+          component: Page500,
         },
         {
-          path: 'login',
-          name: 'Login',
-          component: Login
+          path: "login",
+          name: "Login",
+          component: Login,
         },
         {
-          path: 'register',
-          name: 'Register',
-          component: Register
-        }
-      ]
-    }
-  ]
+          path: "register",
+          name: "Register",
+          component: Register,
+        },
+      ],
+    },
+  ];
 }
 
 const router = new VueRouter({
-  mode: 'history', // https://router.vuejs.org/api/#mode
-  linkActiveClass: 'active',
+  mode: "history", // https://router.vuejs.org/api/#mode
+  linkActiveClass: "active",
   scrollBehavior: () => ({ y: 0 }),
-  routes: configRoutes()
+  routes: configRoutes(),
 });
-
-
 
 export default router;
